bef9bf002bdd5f2889de89830af2605a388cc3ad
Milka Freire <2023102060047@aluno.ifro.edu.br>
2024-11-13 20:38:34 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+try { 
+   Conta c = new Conta();
+    c.SetSaldo(-60);
+
+}catch(Exception ex)
+{
+    Console.WriteLine( ex.Message);
+}
---
+++ conta.cs
@@ -0,0 +1,29 @@
+﻿
+
+public class Conta
+{
+    private double _saldo;
+    public void SetSaldo(double s)
+    {
+        try
+        {
+            if (s >= 0)
+            {
+
+                _saldo = s;
+
+            }
+            else
+            {
+                throw new Exception("o saldo não pode ser negativo");
+            }
+        }catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+            
+    }
+}
+
+
+
